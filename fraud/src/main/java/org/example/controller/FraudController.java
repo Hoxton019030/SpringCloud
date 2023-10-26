418cad87825224a36d0c62a31e4f83a61411533c
Hoxton <hoxton275373327@gmail.com>
2023-10-27 00:40:22 +0800

    成功啟動Eureka

--- fraud/src/main/java/org/example/controller/FraudController.java
+++ fraud/src/main/java/org/example/controller/FraudController.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.response.FraudCheckResponse;
 import org.example.service.FraudCheckService;
 import org.springframework.web.bind.annotation.*;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @RequiredArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
     @GetMapping("{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
+        log.info("顧客id是:{}",customerId);
         boolean fraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
         return FraudCheckResponse.builder().isFraudster(fraudulentCustomer).build();
     }
